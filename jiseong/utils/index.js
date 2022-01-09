6f2237fd81fe404b1490ccda5ed8d7cd96b8a17a
Danji-ya <erangerang15@gmail.com>
2022-01-09 12:25:42 +0900

    utils 추가

---
+++ jiseong/utils/index.js
@@ -0,0 +1,11 @@
+function validateEmail(email){
+    const reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+
+    if (reg.test(email) == false) 
+    {
+        alert('Invalid Email Address');
+        return false;
+    }
+
+    return true;
+}
